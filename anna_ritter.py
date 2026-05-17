113cd80eefb3df6f1f476f408d856f7e8c28a0b5
AnnonymousM <135227844+AnnonymousM@users.noreply.github.com>
2026-05-17 12:33:42 -0300

    Update anna_ritter.py

--- anna_ritter.py
+++ anna_ritter.py
@@ -2,9 +2,22 @@ import random
 possibilidades = [0,10,20]
 
 
-def consequencia(saude: int, energia: int, comida: int, agua: int, sanidade:int) -> int: # 1 = agua 2 = comida 3 = dormir
+def buscar_agua(estado, escolha) -> int: 
   if escolha == 1:
-    status[agua] += 20
-    status[energia] -= 10
-    status[energia] -= possibilidades.choice()
+    estado[agua] += 20
+    estado[energia] -= 10
+    estado[sanidade] -= possibilidades.choice()
+
+def buscar_comida(estado, escolha) -> int: 
+  if escolha == 1:
+    estado[comida] += 20
+    estado[energia] -= 20
+    estado[sanidade] -= possibilidades.choice()
+
+def descansar(estado, escolha) -> int: 
+  if escolha == 1:
+    estado[comida] -= 10
+    estado[agua] -= 10
+    estado[energia] += 20
+    estado[sanidade] += 10
     
