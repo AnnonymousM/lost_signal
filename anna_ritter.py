e9c890e77c738f8e42e6ee81b80d41c6cc6a08c3
AnnonymousM <135227844+AnnonymousM@users.noreply.github.com>
2026-05-17 12:50:57 -0300

    Fix syntax and variable access in anna_ritter.py

--- anna_ritter.py
+++ anna_ritter.py
@@ -1,23 +1,23 @@
-import random 
-possibilidades = [0,10,20]
+import random
 
+possibilidades = [0, 10, 20]
 
-def buscar_agua(estado, escolha) -> int: 
-  if escolha == 1:
-    estado[agua] += 20
-    estado[energia] -= 10
-    estado[sanidade] -= possibilidades.choice()
+def buscar_agua(estado, escolha):
+    if escolha == 1:
+        estado["agua"] += 20
+        estado["energia"] -= 10
+        estado["sanidade"] -= random.choice(possibilidades)
 
-def buscar_comida(estado, escolha) -> int: 
-  if escolha == 1:
-    estado[comida] += 20
-    estado[energia] -= 20
-    estado[sanidade] -= possibilidades.choice()
+def buscar_comida(estado, escolha):
+    if escolha == 1:
+        estado["comida"] += 20
+        estado["energia"] -= 20
+        estado["sanidade"] -= random.choice(possibilidades)
 
-def descansar(estado, escolha) -> int: 
-  if escolha == 1:
-    estado[comida] -= 10
-    estado[agua] -= 10
-    estado[energia] += 20
-    estado[sanidade] += 10
+def descansar(estado, escolha):
+    if escolha == 1:
+        estado["comida"] -= 10
+        estado["agua"] -= 10
+        estado["energia"] += 20
+        estado["sanidade"] += 10
     
