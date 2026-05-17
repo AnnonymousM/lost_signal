1539ea587da194fc8b0e46436caa2daaabf60c7f
AnnonymousM <135227844+AnnonymousM@users.noreply.github.com>
2026-05-17 12:49:02 -0300

    Add menu and status display functions

--- anna_ritter2.py
+++ anna_ritter2.py
@@ -1,1 +1,12 @@
+def mostrar_menu(opcoes):
+    print("O que você vai fazer hoje?")
+    print(opcoes)
+    escolha = input()
 
+def mostrar_status(estado):
+    print("Texto genérico sobre o dia ter acabado")
+    print(f"Saúde: {estado['saude']}\n"
+      f"Energia: {estado['energia']}\n"
+      f"Comida: {estado['comida']}\n"
+      f"Água: {estado['agua']}\n"
+      f"Sanidade: "{estado[sanidade]}) 
