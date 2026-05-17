1fa8f438ac5d39ea6b5f43cbb8e93eeb1d7a5f70
AnnonymousM <135227844+AnnonymousM@users.noreply.github.com>
2026-05-17 12:55:26 -0300

    Update anna_ritter2.py

--- anna_ritter2.py
+++ anna_ritter2.py
@@ -1,12 +1,16 @@
-def mostrar_menu(opcoes):
-    print("O que você vai fazer hoje?")
-    print(opcoes)
-    escolha = input()
+def mostrar_menu(opcoes): #Recebe um número correspondente a uma das escolhas (número dentro da var opcoes)
+    print("O que você vai fazer hoje?") #Exibe o texto pro usuário
+    print(opcoes)                       #Exibe as opções pro usuário
+    escolha = input()                   #Guarda o número escolhido pelo usuário na var escolha
+   
+
 
 def mostrar_status(estado):
-    print("Texto genérico sobre o dia ter acabado")
-    print(f"Saúde: {estado['saude']}\n"
-      f"Energia: {estado['energia']}\n"
-      f"Comida: {estado['comida']}\n"
-      f"Água: {estado['agua']}\n"
-      f"Sanidade: "{estado[sanidade]}) 
+    print("Texto genérico sobre o dia ter acabado") #Exibe o texto pro usuário
+    print(
+        f"Saúde: {estado['saude']}\n"
+        f"Energia: {estado['energia']}\n"
+        f"Comida: {estado['comida']}\n"
+        f"Água: {estado['agua']}\n"
+        f"Sanidade: {estado['sanidade']}"  #Exibe cada um dos status atualizados
+    )
