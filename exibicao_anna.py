29106892adee0e445db2dff4158a38bb6dc599e1
AnnonymousM <135227844+AnnonymousM@users.noreply.github.com>
2026-05-26 10:36:10 -0300

    Update exibicao_anna.py

--- exibicao_anna.py
+++ exibicao_anna.py
@@ -1,8 +1,8 @@
-def mostrar_menu(indice_dias): #Recebe um número correspondente a
-    if indice_dias < 7:
-        print("O que você vai fazer hoje? Digite 1 pra buscar água, 2 pra buscar comida e 3 pra descansar: ")                    
-        escolha = input() 
-        return escolha                
+def mostrar_menu(indice_dias): #Recebe um número correspondente a quantos dias já passaram
+    if indice_dias < 7:        #Enquanto não tiverem passado sete dias, executa o código abaixo
+        print("O que você vai fazer hoje? Digite 1 pra buscar água, 2 pra buscar comida e 3 pra descansar: ")  #Exibe as possíveis escolhas pro player              
+        escolha = input()      #Recebe a escolha do player
+        return escolha         #Retorna a escolha do player      
    
 
 
@@ -19,7 +19,7 @@ def mostrar_texto(indice_dias):
     lore = "Você ficou preso em uma exploração em uma floresta isolada após perder contato com sua equipe. " \
     "Você tem apenas um rádio quebrado e alguns mantimentos em sua mochila, e pela área ser muito isolada da civilização," \
     " a equipe de resgate irá demorar para chegar. Tome decisões, planeje e administre seus recursos, " \
-    "e tente sair da floresta vivo."
+    "e tente sair da floresta vivo."   #Sinopse do jogo
 
     tutorial = tutorial = """
 Você vai começar com:
@@ -36,21 +36,21 @@ buscar comida, buscar água e descansar.
 Mas cuidado! se você zerar energia, você desmaia e perde água e comida.
 Zerar água ou comida leva à morte.
 Zerar saúde, obviamente, leva à morte e sanidade... bem, quem sabe?
-"""
+"""    #Tutorial do jogo 
 
 
-    fim_do_dia = "Está escurecendo e parece que isso é tudo que você vai fazer por hoje."
+    fim_do_dia = "Está escurecendo e parece que isso é tudo que você vai fazer por hoje."  #Texto que aparece depois que o jogador faz a escolha
 
 
-    if indice_dias == 0:
+    if indice_dias == 0: #Condicional pra que a sinopse e o tutorial só apareçam no primeiro ciclo do loop
         print(lore)
         print(tutorial)
 
     else:
-        print(fim_do_dia)
+        print(fim_do_dia)  #Nos demais ciclos, vai exibir o texto de "fim do dia"
 
 
-def verificar_final(estado,sanidade_zerou):
+def verificar_final(estado,sanidade_zerou):   #Recebe o dicionário de estado e a variável pra saber se zerou a sanidade
 
     textos = {
 
@@ -80,21 +80,21 @@ def verificar_final(estado,sanidade_zerou):
         "A realidade se disfaz diante dos seus olhos.\n"
         "Algumas pessoas te tiram daqui e resgatam o que sobrou de você.\n"
         "FINAL RUIM - Eles não vão acreditar em você"
-    }
+    } #Dicionário com textos correspondentes a cada tipo de morte e final
 
     if estado["comida"] == 0:
-        print(textos["morte por fome"])
+        print(textos["morte por fome"])             #condicional que exibe o texto de morte por fome caso o estado "comida" chegue a zero
 
     if estado["agua"] == 0:
-        print(textos["morte por desidratação"])
+        print(textos["morte por desidratação"])     #condicional que exibe o texto de morte por desidratação caso o estado "agua" chegue a zero
 
     if estado["saude"] == 0:
-        print(textos["morte por saúde"])
+        print(textos["morte por saúde"])            #condicional que exibe o texto de morte por saúde caso o estado "saude" chegue a zero
 
-    if sanidade_zerou == True and indice_dias >= 7:
-        print(textos["final ruim"])
+    if sanidade_zerou == True and indice_dias >= 7: #condicional que exibe o texto de final ruim caso o índice de dias seja igual ou maior que sete e sanidade tenha zerado
+        print(textos["final ruim"])                 
     else:
-        print(textos["final bom"])
+        print(textos["final bom"])                   #condicional que exibe o texto de final bom caso nenhum estado chegue a zero antes de 7 ciclos
 
 
 
